8b8b3c17d63aefc86ec9e0e38b6ef8c993ffdaae
Rainhunter13 <rakhmandva@gmail.com>
2021-01-05 18:58:06 +0600

    Product Model added (beta)

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+	category = models.CharField(max_length=60, default="")
+	name = models.CharField(max_length=60, default="")
+
+	def __str__(self):
+		return self.name
